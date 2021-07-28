dfafd1999d3a00a98a6ee8c0562d8d17b431bf01
ani-kesh <aniqeshishyan1992@gmail.com>
2021-07-28 15:38:07 +0400

    feat:add error page design

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,3 +1,57 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import SentimentVeryDissatisfiedIcon from "@material-ui/icons/SentimentVeryDissatisfied";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 500,
+    maxWidth: 700,
+    display:"flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  errorContainer: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    backgroundColor: "#546e7a",
+    padding:"30px 0",
+  },
+  icon: {
+    width: "400px",
+    height: "400px",
+    transform: "rotateX(3.142rad)",
+    color:"white"
+  },
+  errorTitle: {
+    width: "100%",
+    fontSize: "8em",
+    textAlign: "center",
+    fontFamily: "Arial Black",
+    letterSpacing: -"1px",
+    color:"white"
+  },
+});
+
 export default function NotFound() {
-  return <>Error</>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.errorContainer}>
+      <div className={classes.errorTitle}>Error</div>
+        <SentimentVeryDissatisfiedIcon className={classes.icon} />
+    </div>
+  );
 }
